f65db4b6fc2a0f22d08b013be68dd6c0109708c0
Ganesh <ganesh123aidi@gmail.com>
2026-04-02 13:13:40 +0545

    added the category

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
