f5912507f3af5147951fe6962540df6815bf4b32
Ganesh <ganesh123aidi@gmail.com>
2026-04-07 12:43:25 +0545

    blog added

--- blog/models.py
+++ blog/models.py
@@ -15,15 +15,15 @@ STATUS_CHOICES = (
     ('published', 'Published'),
 )   
 class Blog(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=True, blank=True)
     slug = models.SlugField(unique=True)
-    catogory = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.CharField(max_length=100)
     featured_image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     short_description = models.CharField(max_length=255)
     blog_body = models.TextField()
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-    featured_post = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=0)
+    featured_post = models.BooleanField(default=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
