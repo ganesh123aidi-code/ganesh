da0c0b029ee1a951ac6629829f6d41a6432af6de
Ganesh <ganesh123aidi@gmail.com>
2026-04-10 12:52:15 +0545

    updated the blog apps full

--- myproject/views.py
+++ myproject/views.py
@@ -6,8 +6,10 @@ from blog.models import Blog
 def home(request):
     featured_posts = Blog.objects.filter(featured_post=True, status=1).order_by('-created_at')
     posts=Blog.objects.filter(status=1).order_by('-created_at')
+    breaking_news = Blog.objects.filter(featured_post=False, status=1).order_by('-created_at')[:5]
     context = {
         'featured_posts': featured_posts,
-        'posts': posts
+        'posts': posts,
+        'breaking_news': breaking_news,
     }
     return render(request, 'home.html', context)
